61b33691f8f19822215b331e31b10c2c5e6db8fc
vasamtejaswi <162611390+vasamtejaswi@users.noreply.github.com>
2024-12-22 22:39:20 +0530

    Update api.js

---
+++ api.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: 'Error fetching users' });
+  }
+});
+
+router.post('/users', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: 'Error creating user' });
+  }
+});
+
+module.exports = router;
