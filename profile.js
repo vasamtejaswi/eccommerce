dee921f7defa1c81d6707d2142ac2fce3d613e64
vasamtejaswi <162611390+vasamtejaswi@users.noreply.github.com>
2025-01-06 17:39:54 +0530

    Update profile.js

---
+++ profile.js
@@ -0,0 +1,14 @@
+// src/pages/Profile.js
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>My Profile</h1>
+      <p>Here you can update your information.</p>
+      {/* Display user profile info here */}
+    </div>
+  );
+};
+
+export default Profile;
